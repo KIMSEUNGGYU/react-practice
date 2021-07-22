c5b0b792f2d8279e89c8ee691362f4908a985c9f
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-07-22 18:59:04 +0900

    [feat] 임의의 데이터 (상태) 기반 렌더링 기능 구현

--- todo-app/src/components/TodoList.jsx
+++ todo-app/src/components/TodoList.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = () => {
+const TodoList = ({ todos }) => {
   return (
     <div className="TodoList">
-      <TodoListItem />
-      <TodoListItem />
-      <TodoListItem />
+      {todos.map((todo) => (
+        <TodoListItem key={todo.id} todo={todo} />
+      ))}
     </div>
   );
 };
