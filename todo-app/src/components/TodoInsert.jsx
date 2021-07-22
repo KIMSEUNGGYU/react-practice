86a3c1e11f7053c1ec4a11bf30bd02c5a2ec607a
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-07-22 19:16:06 +0900

    [feat] TodoItem 추가 기능 구현

--- todo-app/src/components/TodoInsert.jsx
+++ todo-app/src/components/TodoInsert.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback(({ target }) => {
+    setValue(target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (event) => {
+      onInsert(value);
+      setValue('');
+      event.preventDefault();
+    },
+    [value, onInsert],
+  );
+
   return (
-    <form className="TodoInsert">
-      <input type="text" placeholder="할 일을 입력하세요" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
