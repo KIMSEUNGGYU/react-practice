26c6d2a509b226a1d79d0f69aa3289efe34399ea
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-07-25 18:31:21 +0900

    [feat] 데이터 연동 및 그에 따른 화면 렌더링 기능 구현

--- news-viewer/src/components/NewsList.jsx
+++ news-viewer/src/components/NewsList.jsx
@@ -1,6 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
+
 import NewsItem from './NewsItem';
+
+const NewsList = () => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // async 를 사용하는 함수 따로 선언
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const response = await axios.get(
+          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=bdbc46c5a1ae48d78d42c2c143a877ef',
+        );
+        setArticles(response.data.articles);
+      } catch (error) {
+        console.error(error);
+      }
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  // 로딩 중 일 때
+  if (loading) return <NewsListBlock>로딩 중...</NewsListBlock>;
+
+  // 아직 articles 값이 설정되어 있지 않을 때
+  if (!articles) return null;
+
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -21,19 +61,6 @@ const sampleArticle = {
   description: '내용',
   url: 'https://google.com',
   urlToImage: 'https://via.placeholder.com/160',
-};
-
-const NewsList = () => {
-  return (
-    <NewsListBlock>
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-      <NewsItem article={sampleArticle} />
-    </NewsListBlock>
-  );
 };
 
 export default NewsList;
