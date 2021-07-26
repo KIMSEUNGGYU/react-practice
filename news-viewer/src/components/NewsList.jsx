a83bdea3672e93a5b2d3366b3ae44df8cfebdb37
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-07-26 15:20:29 +0900

    [refactor] api 요청 부분 custom hooks 로 변경

--- news-viewer/src/components/NewsList.jsx
+++ news-viewer/src/components/NewsList.jsx
@@ -3,37 +3,25 @@ import styled from 'styled-components';
 import axios from 'axios';
 
 import NewsItem from './NewsItem';
+import usePromise from '../lib/usePromise';
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    // async 를 사용하는 함수 따로 선언
-    const fetchData = async () => {
-      setLoading(true);
-
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=bdbc46c5a1ae48d78d42c2c143a877ef`,
-        );
-        setArticles(response.data.articles);
-      } catch (error) {
-        console.error(error);
-      }
-      setLoading(false);
-    };
-
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=bdbc46c5a1ae48d78d42c2c143a877ef`,
+    );
   }, [category]);
 
   // 로딩 중 일 때
   if (loading) return <NewsListBlock>로딩 중...</NewsListBlock>;
 
   // 아직 articles 값이 설정되어 있지 않을 때
-  if (!articles) return null;
+  if (!response) return null;
 
+  if (error) return <NewsListBlock>에러 발생!</NewsListBlock>;
+
+  const { articles } = response.data;
   return (
     <NewsListBlock>
       {articles.map((article) => (
