324b8cae0fd66fba182556650100b2bc910dcc40
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-07-27 20:40:47 +0900

    [refactor] counter 컨테이너 간략화

--- react-redux-tutorial/src/modules/counter.js
+++ react-redux-tutorial/src/modules/counter.js
@@ -1,10 +1,14 @@
+import { createAction, handleActions } from 'redux-actions';
+
 // 액션 타입 선언
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
 // 액션 생성 함수 선언
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 // 초기 상태 선언
 const initialState = {
@@ -12,19 +16,27 @@ const initialState = {
 };
 
 // 리듀서 선언
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
+
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return {
+//         number: state.number + 1,
+//       };
+//     case DECREASE:
+//       return {
+//         number: state.number - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// }
 
 export default counter;
