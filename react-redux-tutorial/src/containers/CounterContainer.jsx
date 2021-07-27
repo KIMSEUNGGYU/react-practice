125d3920efe4f1e2a76d4feefab9548809ea058b
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-07-27 18:00:13 +0900

    [feat] counter container 기능 구현

---
+++ react-redux-tutorial/src/containers/CounterContainer.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import Counter from '../components/Counter';
+import { increase, decrease } from '../modules/counter';
+
+const CounterContainer = ({ number, increase, decrease }) => {
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};
+
+export default connect(
+  (state) => ({
+    number: state.counter.number,
+  }),
+  (dispatch) => ({
+    increase: () => dispatch(increase()),
+    decrease: () => dispatch(decrease()),
+  }),
+)(CounterContainer);
+// const mapStateProps = (state) => ({
+//   number: state.counter.number,
+// });
+
+// const mapDispatchToProps = (dispatch) => ({
+//   increase: () => dispatch(increase()),
+//   decrease: () => dispatch(decrease()),
+// });
+
+// export default connect(mapStateProps, mapDispatchToProps)(CounterContainer);
