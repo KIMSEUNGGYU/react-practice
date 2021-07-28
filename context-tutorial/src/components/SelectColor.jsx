5e3296a5eae60751a9db2424212af35aabe6f0c8
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-07-28 23:15:06 +0900

    [refactor] class 컴포넌트 Context 사용 법 - static contextType

--- context-tutorial/src/components/SelectColor.jsx
+++ context-tutorial/src/components/SelectColor.jsx
@@ -1,38 +1,73 @@
-import React from "react";
+import React, { Component } from "react";
 
-import { ColorConsumer } from "../context/color";
+import ColorContext from "../context/color";
 
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
-const SelectColors = () => {
-  return (
-    <div>
-      <h2>색상을 선택하세요.</h2>
-      <ColorConsumer>
-        {({ actions }) => (
-          <div style={{ display: "flex" }}>
-            {colors.map((color) => (
-              <div
-                key={color}
-                style={{
-                  background: color,
-                  width: "24px",
-                  height: "24px",
-                  cursor: "pointer",
-                }}
-                onClick={() => actions.setColor(color)}
-                onContextMenu={(event) => {
-                  event.preventDefault();
-                  actions.setSubColor(color);
-                }}
-              />
-            ))}
-          </div>
-        )}
-      </ColorConsumer>
-      <hr />
-    </div>
-  );
-};
+class SelectColors extends Component {
+  static contextType = ColorContext;
+
+  handleSetColor = (color) => {
+    this.context.actions.setColor(color);
+  };
+  handleSetSubColor = (color) => {
+    this.context.actions.setSubColor(color);
+  };
+
+  render() {
+    return (
+      <div style={{ display: "flex" }}>
+        {colors.map((color) => (
+          <div
+            key={color}
+            style={{
+              background: color,
+              width: "24px",
+              height: "24px",
+              cursor: "pointer",
+            }}
+            onClick={() => this.handleSetColor(color)}
+            onContextMenu={(event) => {
+              event.preventDefault();
+              this.handleSetSubColor(color);
+            }}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+// 함수형
+// const SelectColors = () => {
+//   return (
+//     <div>
+//       <h2>색상을 선택하세요.</h2>
+//       <ColorConsumer>
+//         {({ actions }) => (
+//           <div style={{ display: "flex" }}>
+//             {colors.map((color) => (
+//               <div
+//                 key={color}
+//                 style={{
+//                   background: color,
+//                   width: "24px",
+//                   height: "24px",
+//                   cursor: "pointer",
+//                 }}
+//                 onClick={() => actions.setColor(color)}
+//                 onContextMenu={(event) => {
+//                   event.preventDefault();
+//                   actions.setSubColor(color);
+//                 }}
+//               />
+//             ))}
+//           </div>
+//         )}
+//       </ColorConsumer>
+//       <hr />
+//     </div>
+//   );
+// };
 
 export default SelectColors;
