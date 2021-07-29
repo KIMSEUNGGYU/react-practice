a461d67cf6692926c95b45eb6a3483e840d71526
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-07-29 19:23:29 +0900

    [refactor] 요청 customeThunk 를 만들어서 리팩토링

---
+++ learn-redux-middleware/src/lib/createRequestThunk.js
@@ -0,0 +1,28 @@
+export default function createRequestThunk(type, request) {
+  // 성공 및 실패 액션 타입을 정의
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+
+  return (params) => async (dispatch) => {
+    dispatch({ type }); // 요청 시작
+
+    try {
+      const response = await request(params);
+      dispatch({
+        type: SUCCESS,
+        payload: response.data,
+      }); // 성공
+    } catch (error) {
+      //   console.error(error);
+      dispatch({
+        type: FAILURE,
+        payload: error,
+        error: true,
+      });
+
+      throw error;
+    }
+  };
+}
+
+// 사용 법 createRequestThunk('GET_USERS', api.getUsers)
--- learn-redux-middleware/src/module/sample.js
+++ learn-redux-middleware/src/module/sample.js
@@ -1,6 +1,7 @@
 import { handleActions } from 'redux-actions';
 
 import * as api from '../lib/api';
+import createRequestThunk from '../lib/createRequestThunk';
 
 // 액션 타입 선언
 const GET_POST = 'sample/GET_POST';
@@ -13,43 +14,44 @@ const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 
 // thunk 함수 생성
 // thunk 함수 내부에서 시작할 때, 성공, 실패할 때 다른액션을 디스패치 함.
-export const getPost = (id) => async (dispatch) => {
-  dispatch({ type: GET_POST }); // 요청 시작
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+// export const getPost = (id) => async (dispatch) => {
+//   // dispatch({ type: GET_POST }); // 요청 시작
+//   // try {
+//   //   const response = await api.getPost(id);
+//   //   dispatch({
+//   //     type: GET_POST_SUCCESS,
+//   //     payload: response.data,
+//   //   });
+//   // } catch (error) {
+//   //   dispatch({
+//   //     type: GET_POST_FAILURE,
+//   //     payload: error,
+//   //     error: true,
+//   //   });
+//   //   throw error; // 나중에 컴포넌트단에서 에러를 조회 할 수 있게 해 줌
+//   // }
+// };
 
-  try {
-    const response = await api.getPost(id);
-    dispatch({
-      type: GET_POST_SUCCESS,
-      payload: response.data,
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_POST_FAILURE,
-      payload: error,
-      error: true,
-    });
-    throw error; // 나중에 컴포넌트단에서 에러를 조회 할 수 있게 해 줌
-  }
-};
+// export const getUsers = (id) => async (dispatch) => {
+//   dispatch({ type: GET_USERS }); // 요청 시작
 
-export const getUsers = (id) => async (dispatch) => {
-  dispatch({ type: GET_USERS }); // 요청 시작
-
-  try {
-    const response = await api.getUsers(id);
-    dispatch({
-      type: GET_USERS_SUCCESS,
-      payload: response.data,
-    });
-  } catch (error) {
-    dispatch({
-      type: GET_USERS_FAILURE,
-      payload: error,
-      error: true,
-    });
-    throw error; // 나중에 컴포넌트단에서 에러를 조회 할 수 있게 해 줌
-  }
-};
+//   try {
+//     const response = await api.getUsers(id);
+//     dispatch({
+//       type: GET_USERS_SUCCESS,
+//       payload: response.data,
+//     });
+//   } catch (error) {
+//     dispatch({
+//       type: GET_USERS_FAILURE,
+//       payload: error,
+//       error: true,
+//     });
+//     throw error; // 나중에 컴포넌트단에서 에러를 조회 할 수 있게 해 줌
+//   }
+// };
 
 // 초기 상태 선언
 const initialState = {
